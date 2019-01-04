131cc3d4a426ed217cdb89389206073acf44b6f1
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-04 21:27:12 +0200

    Create Header component

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    const {name, position, born, email, mobile, education} = this.props
+    return (
+      <div>
+        <h1>{name}</h1>
+        <span>{position}</span>
+        <br/><br/>
+        <span>
+          <b>Born:</b> {born}
+        </span><br/>
+        <span>
+          <b>Email:</b> <a href={`mailto:${email}`}>{email}</a>
+        </span><br/>
+        <span>
+          <b>Mobile:</b> {mobile}
+        </span><br/>
+        <span>
+          <b>Education:</b>  {education}
+        </span>
+      </div>
+    );
+  }
+}
+
+export default Header;
