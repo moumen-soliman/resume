26e0e2e3347b69a87bc9ff8023ad4a401a1c9369
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-04 21:27:37 +0200

    Import and throw props from json file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import './assets/style/app.scss';
-import stackOverFlow from './assets/img/stackoverflow.png';
-import gitHub from './assets/img/github.svg';
-import linkedIn from './assets/img/linkedin.png';
-import medium from './assets/img/medium.png';
-import hackerRank from './assets/img/hackerrank.png';
+import resume from './resume.json';
+import Header from './components/Header';
+import SocialItems from './components/SocialItems';
 
 class App extends Component {
   render() {
@@ -12,39 +10,14 @@ class App extends Component {
       <div className="container">
         <div className="app">
           <div className="app__header">
-            <h1>Moumen Soliman</h1>
-            <span>Front-end Developer</span>
             <div className="app__header__info">
               <div className="col-md-12">
-                <span>
-                  <b>Born:</b> 19 Oct 1995 / Cairo, Egypt
-                </span><br/>
-                <span>
-                  <b>Email:</b> <a href="mailto:moumensoliman@gmail.com">moumensoliman@gmail.com</a>
-                </span><br/>
-                <span>
-                  <b>Mobile:</b> +201128412656
-                </span><br/>
-                <span>
-                  <b>Education:</b>  Cairo higher Institute - Computer Science
-                </span>
+                  <Header {...resume.personal} />
               </div>
               <div className="social-items col-md-12">
-                <a className="social-item col-md-1" href="https://stackoverflow.com/users/8960519/moumen-soliman" target="_blank" rel="noopener noreferrer">
-                  <img src={stackOverFlow} alt="stackoverflow logo"/>
-                </a>
-                <a className="social-item col-md-1" href="https://github.com/moumen-soliman" target="_blank" rel="noopener noreferrer">
-                  <img src={gitHub} alt="github logo"/>
-                </a>
-                <a className="social-item col-md-1" href="https://www.linkedin.com/in/moumensoliman/" target="_blank" rel="noopener noreferrer">
-                  <img src={linkedIn} alt="linkedin logo"/>
-                </a>
-                <a className="social-item col-md-1" href="https://medium.com/@moumensoliman" target="_blank" rel="noopener noreferrer">
-                  <img src={medium} alt="medium logo"/>
-                </a>
-                <a className="social-item col-md-1" href="https://hackerrank.com/moumensoliman" target="_blank" rel="noopener noreferrer">
-                  <img src={hackerRank} alt="hackerrank logo"/>
-                </a>
+                {
+                  resume.social.map(social => <SocialItems key={social.id} {...social} />)
+                }
               </div>
             </div>
           </div>
