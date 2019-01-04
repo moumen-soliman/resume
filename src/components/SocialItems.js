36b1154097b5083da8804a9181a2262c681c85e5
Mo'men Soliman <moumensoliman@gmail.com>
2019-01-04 21:27:24 +0200

    Create Social Items component

---
+++ src/components/SocialItems.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SocialItems extends Component {
+  render() {
+    const {name, link, logo} = this.props
+    return (
+        <a className="social-item col-md-1" href={link} target="_blank" rel="noopener noreferrer">
+          <img src={logo} alt={`${name} logo`}/>
+        </a>
+    );
+  }
+}
+
+export default SocialItems;
