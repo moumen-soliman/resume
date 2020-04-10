424c9a7b484b009c2ad9cf2da95a3a9f7891572f
moumen-soliman <moumensoliman@gmail.com>
2020-04-10 17:16:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import './assets/style/app.scss';
-import resume from './resume.json';
-import Award from './components/Award';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Education from './components/Education';
-import Volunteer from './components/Volunteer';
-import Experience from './components/Experience';
-import SocialItems from './components/SocialItems';
-import Certificate from './components/Certificate';
-import Contributions from './components/Contributions';
+import React, { Component } from "react";
+
+import resume from "./resume.json";
+import Award from "./components/Award";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Volunteer from "./components/Volunteer";
+import Experience from "./components/Experience";
+import SocialItems from "./components/SocialItems";
+import Certificate from "./components/Certificate";
+import Contributions from "./components/Contributions";
+
+import "./assets/style/app.scss";
 
 class App extends Component {
   render() {
@@ -22,47 +24,47 @@ class App extends Component {
                 <Header {...resume.personal} />
               </div>
               <div className="social-items col-md-4">
-                {
-                  resume.social.map(social => <SocialItems key={social.id} {...social} />)
-                }
+                {resume.social.map(social => (
+                  <SocialItems key={social.id} {...social} />
+                ))}
               </div>
             </div>
           </div>
           <div className="app__experience pd-btm">
             <h3 class="head-title let-spc">Employment</h3>
-            {
-              resume.experience.map(exp => <Experience key={exp.id} {...exp} />)
-            }
+            {resume.experience.map(exp => (
+              <Experience key={exp.id} {...exp} />
+            ))}
           </div>
           <div className="app__education pd-btm">
             <h3 class="head-title let-spc">Education</h3>
-            {
-              <Education {...resume.education} />
-            }
+            {<Education {...resume.education} />}
           </div>
           <div className="app__experience pd-btm">
             <h3 class="head-title let-spc">Contributions</h3>
-            {
-              resume.contributions.map(exp => <Contributions key={exp.id} {...exp} />)
-            }
+            {resume.contributions.map(exp => (
+              <Contributions key={exp.id} {...exp} />
+            ))}
           </div>
           <div className="app__projects pd-btm">
             <h3 class="head-title let-spc">Projects & Packages</h3>
-            {
-              resume.projects.map(proj => <Projects key={proj.id} {...proj} />)
-            }
+            {resume.projects.map(proj => (
+              <Projects key={proj.id} {...proj} />
+            ))}
           </div>
           <div className="app__certificate pd-btm">
             <h3 class="head-title let-spc">Certificate</h3>
-            {
-              resume.certificates.map(certificate => <Certificate key={certificate.id} {...certificate} />)
-            }
+            <ul>
+              {resume.certificates.map(certificate => (
+                <Certificate key={certificate.id} {...certificate} />
+              ))}
+            </ul>
           </div>
           <div className="app__award pd-btm">
             <h3 class="head-title let-spc">Additional Experience and Awards</h3>
-            {
-              resume.awards.map(award => <Award key={award.id} {...award} />)
-            }
+            {resume.awards.map(award => (
+              <Award key={award.id} {...award} />
+            ))}
           </div>
           <div className="app__languages pd-btm">
             <h3 class="head-title let-spc">Languages and Technologies</h3>
@@ -70,9 +72,9 @@ class App extends Component {
           </div>
           <div className="app__award pd-btm">
             <h3 class="head-title let-spc">Volunteer Experience & Activites</h3>
-            {
-              resume.volunteering.map(vol => <Volunteer key={vol.id} {...vol} />)
-            }            
+            {resume.volunteering.map(vol => (
+              <Volunteer key={vol.id} {...vol} />
+            ))}
           </div>
         </div>
       </div>
