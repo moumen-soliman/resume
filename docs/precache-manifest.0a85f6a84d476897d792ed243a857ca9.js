4e667dbebe9a40330a9041dac35255bdd65708eb
moumen-soliman <moumensoliman@gmail.com>
2020-04-10 19:22:37 +0200

    update docs file

---
+++ docs/precache-manifest.0a85f6a84d476897d792ed243a857ca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c470cef96c3b06c698fa3f1d339fcd9",
+    "url": "/static/media/sun.1c470cef.svg"
+  },
+  {
+    "revision": "275c3afd156cc1de967977f0fbcf8d26",
+    "url": "/static/media/moon.275c3afd.svg"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "07dbae5c636172dd76fd",
+    "url": "/static/js/main.07dbae5c.chunk.js"
+  },
+  {
+    "revision": "a5fd0e811ec541a20cd0",
+    "url": "/static/js/2.a5fd0e81.chunk.js"
+  },
+  {
+    "revision": "07dbae5c636172dd76fd",
+    "url": "/static/css/main.b6f751e6.chunk.css"
+  },
+  {
+    "revision": "385077a69d997ef2f0f457ff23d9b7dc",
+    "url": "/index.html"
+  }
+];
